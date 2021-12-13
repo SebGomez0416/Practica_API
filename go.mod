7c112679bf08fc59ff2907ada458dd94e4240b78
juan sebastian gomez <sebbastian.gc@gmail.com>
2021-12-13 13:13:25 -0300

    add connection to the database

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/SebGomez0416/Practica_API
+
+go 1.17
+
+require github.com/lib/pq v1.10.4
---
+++ storage/storage.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"sync"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	db   *sql.DB
+	once sync.Once
+)
+
+//NewPostgresDB  open Data Base
+func NewPostgresDB() {
+	once.Do(func() {
+		var err error
+
+		db, err = sql.Open("postgres", "postgres://postgres:seb95025@localhost:5432/practica_API?sslmode=disable")
+		if err != nil {
+			log.Fatalf("can't open DB: %v ", err)
+		}
+		if err = db.Ping(); err != nil {
+			log.Fatalf("can't do ping: %v ", err)
+		}
+		fmt.Println("conectado a postgres")
+
+	})
+
+}
+
+// pool reutrn a unique instance of db
+func Pool() *sql.DB {
+	return db
+}
