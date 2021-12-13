9453962647ffaa49fdccb3caaf3d349c4e933821
juan sebastian gomez <sebbastian.gc@gmail.com>
2021-12-13 11:17:40 -0300

    add struct person

---
+++ model/person.go
@@ -0,0 +1,11 @@
+package model
+
+//  person estructura de una persona
+type Person struct {
+	Name          string
+	Age           uint8
+	CommunityName string
+}
+
+// Persons slice de personas
+type Persons []Person
