fab7b9285645b6302831ee368b763819696acd9c
juan sebastian gomez <sebbastian.gc@gmail.com>
2021-12-16 14:52:38 -0300

    add Handler delete

--- handler/person.go
+++ handler/person.go
@@ -82,3 +82,71 @@ func (p *person) getAll(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+func (p *person) update(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"message_type" : "error" , "message" : "Metodo no permitido"}`))
+		return
+	}
+
+	data := model.Person{}
+
+	err := json.NewDecoder(r.Body).Decode(&data)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"message_type" : "error" , "message" : "la persona no fue cargada correctamente"}`))
+		return
+	}
+
+	err = p.storage.Update(&data)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"message_type" : "error" , "message" : "Hubo un problema al actualizar la persona"}`))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message_type" : "message" , "message" : "persona actualizada  correctamente"}`))
+
+}
+
+func (p *person) delete(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"message_type" : "error" , "message" : "Metodo no permitido"}`))
+		return
+	}
+
+	data := model.Person{}
+
+	err := json.NewDecoder(r.Body).Decode(&data)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"message_type" : "error" , "message" : "la persona no fue cargada correctamente"}`))
+		return
+	}
+
+	err = p.storage.Delete(data.ID)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"message_type" : "error" , "message" : "Hubo un problema al eliminar la persona"}`))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message_type" : "message" , "message" : "persona eliminada  correctamente"}`))
+
+}
